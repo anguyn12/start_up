31f67eb8eaa93d03de693bb7f354a45d2fcae347
Dawn <anhnguyen19903@gmail.com>
2018-01-30 17:17:10 -0600

    launch

---
+++ EggApp/EggApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EggApp
+//
+//  Created by Dawn Nguyen on 1/30/18.
+//  Copyright © 2018 StartUP. All rights reserved.
+//
+
+import UIKit
+import Lottie
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
