d73f79ef101c84136fcef526b1ed9a6e8e65da4a
GuanHao Yan <gyan5@illinois.edu>
2018-02-06 18:45:03 -0600

    modified server folders

--- EggApp/server/main.py
+++ EggApp/server/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
-
+from eggAppServer import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(threaded=True)
