1957f22a17bec8f28a7d8856e1b22abe8be81664
GuanHao Yan <gyan5@illinois.edu>
2018-02-06 18:45:42 -0600

    added eggappserver folder

---
+++ EggApp/server/eggAppServer/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+def create_app(cfg=None, cors=False):
+    app = Flask(__name__)
+    if(cors==True):
+        CORS(app)
+
+    from api.user import api_user
+    app.register_blueprint(api_user, url_prefix = '/')
+
+    return app
